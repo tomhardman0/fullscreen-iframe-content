20ce84ad97778642a650d41aa557504b900bdfd9
Tom Hardman <tomhardman0@gmail.com>
2015-08-03 17:01:18 +0100

    another test

--- index.js
+++ index.js
@@ -4,29 +4,43 @@ exports.fullscreenContent = function(container, iframe, aspectX, aspectY) {
   var container = document.querySelectorAll(container)[0];
   var iframe = document.querySelectorAll(iframe)[0];
 
-  // Measure dimensions and calculate correct size
-  var width = window.innerWidth
-  var height = window.innerHeight
-  var wide = aspectX / aspectY;
-  var aspect = width / height;
-  var multiRatio = wide / aspect
+  container.parentNode.style.overflow = 'hidden';
+  container.style.position = 'absolute';
+  container.style.top = '50%';
+  container.style.left = '50%';
+  container.style.transform = 'translate(-50%,-50%)';
 
-  if (multiRatio >= 1) {
-    width *= multiRatio
-    height *= multiRatio
-  } else {
-    width /= multiRatio
-    height /= multiRatio
-    }
 
-  container.style.height = height.toString() + 'px';
-  container.style.width = width.toString() + 'px';
-  iframe.getAttributeNode('width').value = '100%';
-  iframe.getAttributeNode('height').value = '100%';
+  var iframeResize = function(container, iframe, aspectX, aspectY) {
 
-  // fullscreenContent()
-  // window.on('resize', function() {
-  // fullscreenContent()
-  // })
+    // Measure dimensions and calculate correct size
+    var width = window.innerWidth
+    var height = window.innerHeight
+    var wide = aspectX / aspectY;
+    var aspect = width / height;
+    var multiRatio = wide / aspect
+
+
+    if (multiRatio >= 1) {
+      width *= multiRatio
+      height *= multiRatio
+    } else {
+      width /= multiRatio
+      height /= multiRatio
+      }
+
+    // Style container, parent, and iFrame
+    container.style.height = height.toString() + 'px';
+    container.style.width = width.toString() + 'px';
+    iframe.getAttributeNode('width').value = '100%';
+    iframe.getAttributeNode('height').value = '100%';
+
+  }
+
+  window.on('load resize', function() {
+
+    iframeResize()
+
+  })
 
 }
