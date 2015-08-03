bb62079f517e8d5a1837b253d5fee6fe0c1fae3c
Tom Hardman <tomhardman0@gmail.com>
2015-08-03 15:36:33 +0100

    initial commit... 2

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-exports.fullscreenContent = function() {
-  
+exports.fullscreenContent = function(container, iframe, aspectX, aspectY) {
+
+  // Select DOM elements
+  var container = document.querySelectorAll(container)[0];
+  var iframe = document.querySelectorAll(iframe)[0];
+
+  // Measure dimensions and calculate correct size
+  var width = window.innerWidth
+  var height = window.innerHeight
+  var wide = aspectX / aspectY;
+  var aspect = width / height;
+  var multiRatio = wide / aspect
+
+  if (multiRatio >= 1) {
+    width *= multiRatio
+    height *= multiRatio
+  } else {
+    width /= multiRatio
+    height /= multiRatio
+    }
+
+  container.style.height = height;
+  container.style.width = width;
+  iframe.getAttributeNode('width').value = '100%';
+  iframe.getAttributeNode('height').value = '100%';
+
+  fullscreenContent()
+  window.on('resize', function() {
+  fullscreenContent()
+  })
+
 }
